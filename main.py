f010cb736c6bff5c549e5ef2ca5adc22a02b0710
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2022-08-30 18:56:52 -0600

    Cambios

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ r = Render(1300, 1300)
 
 t = Texture('model.bmp')
 
-r.load('model.obj', (1, 0.5, 0), (700, 700, 700), texture=t)
+r.load('model.obj', (1, 1, 1), (300, 300, 300), texture=t)
 
 r.glFinish('output.bmp')
--- textures.py
+++ textures.py
@@ -63,7 +63,7 @@ class Texture(object):
             color(r, g, b)
           )
 
-  def get_color(self, tx, ty):
+  def get_color(self, tx, ty, intensity):
       x = round(tx*self.width)
       y = round(ty*self.heigth)
 
@@ -73,9 +73,9 @@ class Texture(object):
       x = round(tx*(self.width-1))
       y = round(ty*(self.heigth-1))
 
-      b = round(self.pixels[y][x][0]*intensity)
-      g = round(self.pixels[y][x][1]*intensity)
-      r = round(self.pixels[y][x][2]*intensity)
+      b = round(self.pixels[y][x][0])
+      g = round(self.pixels[y][x][1])
+      r = round(self.pixels[y][x][2])
 
       b = max(0, min(b, 255))
       g = max(0, min(g, 255))
